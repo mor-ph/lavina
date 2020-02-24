bcd73fdc3f0142a8be661aec2061a7f2186d49ff
DimitarVasilevBG <mitko.v@mail.bg>
2020-02-24 11:31:57 +0200

    Event API commit

---
+++ EventService/EventAPI/Models/Event.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventAPI.Models
+{
+    public enum Status
+    {
+        
+       
+        Created = 1,
+        Repeating = 2,
+        Finished = 3,
+
+
+    }
+    public class Event
+    {
+
+        [Key]
+        public int ID { get; set; }
+        public DateTime EventStartDate { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedOn { get; set; } = DateTime.UtcNow;
+        public Status EventStatus { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public int PeopleNeeded { get; set; }
+        public string Title { get; set; }
+        public int CreatedBy { get; set; }
+
+        
+
+
+    }
+}
