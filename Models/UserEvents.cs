e9f2fbf82492ec4ff4df5a7704e5f8b850586d43
Gabriela <yet.an0ther.mortal@gmail.com>
2020-02-25 15:01:40 +0200

    deleting UserEvents

--- Models/UserEvents.cs
+++
@@ -1,9 +0,0 @@
-﻿
-// table for communication of foreign keys
-
-public class UserEvents
-{
-    public int ID { get; set; }
-    public int UserID { get; set; }
-    public int EventID { get; set; }
-}
