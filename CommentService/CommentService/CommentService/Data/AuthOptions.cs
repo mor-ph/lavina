704fa81f6012cf35790bed1734ec32a4cb3db53a
Aleksey Nikolaev <nikolaev.profent@gmail.com>
2020-03-02 15:05:03 +0200

    add AuthOptions.cs

---
+++ CommentService/CommentService/CommentService/Data/AuthOptions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentService.Data
+{
+    public class AuthOptions
+    {
+        const string KEY = "cAtwa1kkEy";   // key for encoding
+        public const int LIFETIME = 999; // token lifetime
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
