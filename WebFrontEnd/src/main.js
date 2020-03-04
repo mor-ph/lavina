64592da6a9796e30d82059965e8f3aef620f9556
PetarParushev <lightone3591@gmail.com>
2020-03-04 14:56:15 +0200

    add vuelidate

--- WebFrontEnd/src/main.js
+++ WebFrontEnd/src/main.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+
 import router from './router'
 import store from './store'
 
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vuelidate from 'vuelidate'
+
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
