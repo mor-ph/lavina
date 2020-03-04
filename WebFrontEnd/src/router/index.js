f2a22b22d812549c02badc9001d7c5165e97bebc
PetarParushev <lightone3591@gmail.com>
2020-03-04 16:44:17 +0200

    Update routes

--- WebFrontEnd/src/router/index.js
+++ WebFrontEnd/src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import CreateEvent from '../views/CreateEvent.vue'
-import Profile from '../views/Profile.vue'
+import Home from '../components/Events/Home.vue'
+import CreateEvent from '../components/CreateEvent/CreateEvent.vue'
+import Profile from '../components/Profile/Profile.vue'
 
 Vue.use(VueRouter)
 
