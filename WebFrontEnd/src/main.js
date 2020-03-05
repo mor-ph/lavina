5a011af851caa633a44875f152719e09e4196637
PetarParushev <lightone3591@gmail.com>
2020-03-05 12:42:55 +0200

    Add Vuelidate

--- WebFrontEnd/src/main.js
+++ WebFrontEnd/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import Vuelidate from 'vuelidate'
 
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -10,6 +12,10 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(Vuelidate)
+
+// TODO: Waiting for docker to add DB directory
+axios.defaults.baseURL = ''
 
 Vue.config.productionTip = false
 
