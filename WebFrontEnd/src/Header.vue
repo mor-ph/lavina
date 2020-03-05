ca2c014be6f649d760a92c8f32d3fb57b8b6ca70
S22v22S <39414953+Stepan2215@users.noreply.github.com>
2020-03-05 17:12:48 +0200

    Update Header.vue

--- WebFrontEnd/src/Header.vue
+++ WebFrontEnd/src/Header.vue
@@ -1,65 +1,67 @@
 <template>
   <!-- Navbar -->
   <b-container fluid>
-    <b-row>
-      <b-navbar toggleable="lg" type="dark" variant="success" class="fixed-top">
-        <b-col sm="5">
-          <b-navbar-brand href="#">
-            <strong>LetsPlay</strong>
-          </b-navbar-brand>
-        </b-col>
+    <div>
+      <b-row>
+        <b-navbar toggleable="lg" type="dark" variant="info" class="fixed-top justify-content-between">
+          <router-link to="/"><b-navbar-brand>Let's Play</b-navbar-brand></router-link>
 
-        <b-col sm="5">
-          <b-button-toolbar>
-            <b-button-group>
-              <router-link to="/">
-                <b-button title="Category" class="colorbutton">
-                  <b-icon icon="filter" font-scale="2.5" aria-hidden="true"></b-icon>
-                </b-button>
-              </router-link>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-              <router-link to="/createEvent">
-                <b-button title="CreatEvent" class="colorbutton">
-                  <b-icon icon="plus" font-scale="2.5" aria-hidden="true"></b-icon>
-                </b-button>
-              </router-link>
-
-              <router-link to="/profile">
-                <b-button title="Profil" class="colorbutton">
-                  <b-icon icon="person-fill" font-scale="2.5" aria-hidden="true"></b-icon>
-                </b-button>
-              </router-link>
-            </b-button-group>
-          </b-button-toolbar>
-        </b-col>
-
-        <!-- Right aligned nav items -->
-
-        <b-col sm="2">
-          <b-navbar-nav class="ml-auto">
-            <b-navbar-nav>
-              <div>
-                <router-link to="/login">
-                  <b-button class="colorbutton"><strong>Login</strong></b-button>
-                </router-link>
-                <router-link to="/register">
-                  <b-button class="colorbutton"><strong>Register</strong></b-button>
-                </router-link>
-              </div>
-            </b-navbar-nav>
+          <b-navbar-nav center>
+            <router-link to="/createEvent">
+            <b-button class="mb-2" size="lg">
+              Create Event
+              <b-icon icon="plus"></b-icon>
+            </b-button>
+            </router-link>
           </b-navbar-nav>
-        </b-col>
-      </b-navbar>
-    </b-row>
+
+          <div>
+            <router-link to="/login">
+              <b-button class="colorbutton">
+                <strong>Login</strong>
+              </b-button>
+            </router-link>
+            <router-link to="/register">
+              <b-button class="colorbutton">
+                <strong>Register</strong>
+              </b-button>
+            </router-link>
+
+            <b-collapse id="nav-collapse" is-nav>
+              <!-- Right aligned nav items -->
+              <b-navbar-nav class="mr-auto">
+                <b-nav-item-dropdown right>
+                  <!-- Using 'button-content' slot -->
+                  <template v-slot:button-content>
+                    <em>
+                      <strong>Username</strong>
+                    </em>
+                  </template>
+                  <router-link to="/profile">
+                  <b-dropdown-item>
+                    Profile
+                    <b-icon icon="person-fill"></b-icon>
+                  </b-dropdown-item>
+                  </router-link>
+                  <b-dropdown-item href="#">
+                    Sign Out
+                    <b-icon icon="box-arrow-right"></b-icon>
+                  </b-dropdown-item>
+                </b-nav-item-dropdown>
+              </b-navbar-nav>
+            </b-collapse>
+          </div>
+        </b-navbar>
+      </b-row>
+    </div>
   </b-container>
 </template>
+
 
 <script>
 </script>
 
 <style>
-.colorbutton{
- background: rgb(33,62,62);
-background: linear-gradient(180deg, rgba(33,62,62,1) 0%, rgba(0,215,255,1) 100%);
-}
 </style>
