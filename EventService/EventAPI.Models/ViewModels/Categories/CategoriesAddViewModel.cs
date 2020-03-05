6d7608183f7a77f447c414575933bbd517e60d8b
Yovcho <ivelinjilov16@gmail.com>
2020-03-05 17:07:57 +0200

    connectionstring change

--- EventService/EventAPI.Models/ViewModels/Categories/CategoriesAddViewModel.cs
+++ EventService/EventAPI.Models/ViewModels/Categories/CategoriesAddViewModel.cs
@@ -11,6 +11,6 @@ namespace EventAPI.Models.ViewModels.Categories
         [MinLength(3,ErrorMessage = "Name cannot be less than {0}")]
         [MaxLength(30,ErrorMessage = "Name cannot be more than {0}")]
         public string Name { get; set; }
-        public int ParentCategoryId { get; set; }
+        public int? ParentCategoryId { get; set; }
     }
 }
