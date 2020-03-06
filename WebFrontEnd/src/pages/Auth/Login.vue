75bb6e50b1e1d4468f294a8536595a5fed5033ec
PetarParushev <lightone3591@gmail.com>
2020-03-06 19:50:16 +0200

    Update Login.vue

--- WebFrontEnd/src/pages/Auth/Login.vue
+++ WebFrontEnd/src/pages/Auth/Login.vue
@@ -1,36 +1,49 @@
 <template>
   <div id="signin">
-    <b-form @submit.prevent="onSubmit" style="padding: 20%">
-      <b-form-group
-        id="username-input-group"
-        label="Username:"
-        label-for="username-input"
-        >
-        <b-form-input
-          id="username-input"
-          type="text"
-          required
-          placeholder="Enter Username"
-          v-model="username"
-        ></b-form-input>
-      </b-form-group>
+    <div class="body">
+      <b-container>
+        <b-row>
+            <b-col sm="5"></b-col>
+            <b-col sm="2">
+        <h1 class="hhh">Login-in</h1>
+        <br> <b-icon icon="person-fill" variant="light" font-scale="10"></b-icon>
+        </b-col>
+         </b-row>
 
-      <b-form-group
-       id="password-input-group"
-       label="Password:"
-       label-for="password-input"
-       >
-        <b-form-input
-          id="password-input"
-          type="password"
-          required
-          placeholder="Enter Password"
-          v-model="password"
-        ></b-form-input>
-      </b-form-group>
+        <b-form @submit.prevent="onSubmit" style="padding: 6%">
+          <b-row>
+            <b-col sm="3"></b-col>
+            <b-col sm="6">
+              <b-form-group id="username-input-group" label="Username:" label-for="username-input" color="white">
+                <b-form-input
+                  id="username-input"
+                  type="text"
+                  required
+                  placeholder="Enter Username"
+                  v-model="username"
+                ></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col sm="3"></b-col>
+            <b-col sm="6">
+          <b-form-group id="password-input-group" label="Password:" label-for="password-input">
+            <b-form-input
+              id="password-input"
+              type="password"
+              required
+              placeholder="Enter Password"
+              v-model="password"
+            ></b-form-input>
+          </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      </b-form>
+          <b-button type="submit" variant="primary">Submit</b-button>
+           </b-col>
+          </b-row>
+        </b-form>
+      </b-container>
+    </div>
   </div>
 </template>
 
@@ -57,5 +70,18 @@ export default {
 </script>
 
 <style scoped>
+.body {
+  background: #f7f7f7;
+  background: rgb(63, 94, 251);
+  background: radial-gradient(
+    circle,
+    rgba(63, 94, 251, 1) 0%,
+    rgba(0, 0, 0, 1) 100%
+  );
+}
+.hhh{
+  padding-top: 130px;
+  color: white;
+}
 
 </style>
