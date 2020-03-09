49cfc46a3d13c8e6c80ac32188e40a25449d3fc4
PetarParushev <lightone3591@gmail.com>
2020-03-09 16:07:29 +0200

    Add Home Page Components

---
+++ WebFrontEnd/src/components/Event/EventsGrid.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <app-event :key="event.id" v-for="event in events" :event="event"></app-event>
+    </div>
+</template>
+
+<script>
+import Event from './Event'
+export default {
+  props: ['events'],
+  components: {
+    appEvent: Event
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
