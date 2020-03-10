4c6092ed9d8bfd9bf03759001c3bb1c3931f9d49
S22v22S <39414953+Stepan2215@users.noreply.github.com>
2020-03-10 15:28:43 +0200

    Change Responsive

--- WebFrontEnd/src/pages/CreateEvent/CreateEvent.vue
+++ WebFrontEnd/src/pages/CreateEvent/CreateEvent.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
     <!-- Color fon -->
-    <div class="body" style="padding-bottom:10%">
+    <div class="body">
       <b-container fluid>
         <!-- Title -->
-        <b-row class="my-3">
-          <b-col sm="4"></b-col>
-
-          <b-col sm="4">
-            <div class="text-center" style="padding:5%">
-              <label for="example-i18n-picker" class="text-black">
+        <b-row class="text-center my-2">
+          <b-col sm="4" offset-sm="4">
+            <div class="text-center  my-2">
+              <label for="example-i18n-picker" class="text-white">
                 <strong>Title:</strong>
               </label>
             </div>
@@ -24,12 +22,11 @@
         </b-row>
 
         <!-- Category -->
-        <b-row class="my-3">
-          <b-col sm="4"></b-col>
+        <b-row class="text-center my-2">
 
-          <b-col sm="4">
+          <b-col sm="4" offset-sm="4">
             <div class="text-center">
-              <label for="example-category" class="text-black">
+              <label for="example-category" class="text-white">
                 <strong>Category:</strong>
               </label>
             </div>
@@ -41,22 +38,18 @@
         </b-row>
 
         <!-- Subcategory -->
-        <b-row class="my-3">
-          <b-col sm="5"></b-col>
-
-          <b-col sm="2">
+        <b-row class="text-center my-2">
+          <b-col sm="2" offset-sm="5">
             <div class="text-center">
-              <label for="example-subcategorys" class="text-black">
+              <label for="example-subcategorys" class="text-white">
                 <strong>Subcategories:</strong>
               </label>
             </div>
           </b-col>
         </b-row>
 
-        <b-row class="my-3">
-          <b-col sm="4"></b-col>
-
-          <b-col sm="3">
+        <b-row class="text-center my-2">
+          <b-col sm="3" offset-sm="4">
             <b-form-select id="example-subcategorys"></b-form-select>
           </b-col>
 
@@ -91,74 +84,65 @@
         </b-row>
 
         <!--  Date & Time picker -->
-        <b-row class="my-3">
-          <b-col sm="4"></b-col>
-
-          <b-col sm="4">
-            <div class="text-center">
-              <label for="example-i18n-picker" class="text-black">
+        <b-row class="text-center my-2">
+          <b-col sm="4" offset-sm="4">
+            <div class="text-center my-2">
+              <label for="example-i18n-picker" class="text-white">
                 <strong>Date & Time picker:</strong>
               </label>
             </div>
           </b-col>
         </b-row>
 
-        <b-row class="my-2">
-          <b-col sm="4"></b-col>
-          <b-col sm="4">
+        <b-row class="text-center my-2">
+          <b-col sm="4" offset-sm="4">
             <b-form-datepicker id="datepicker-valid" :state="true"></b-form-datepicker>
           </b-col>
         </b-row>
-        <b-row class="my-2">
-          <b-col sm="4"></b-col>
-          <b-col sm="4">
-          <b-form-timepicker id="datepicker-valid" :state="true"></b-form-timepicker>
+        <b-row class="text-center my-2">
+          <b-col sm="4" offset-sm="4">
+          <b-form-timepicker id="timepicker-valid" :state="true"></b-form-timepicker>
          </b-col>
         </b-row>
         <!-- Location -->
-        <b-row class="my-3">
-          <b-col sm="4"></b-col>
+        <b-row class="text-center my-2">
 
-          <b-col sm="4">
-            <div class="text-center">
-              <label for="example-location" class="text-black">
+          <b-col sm="4" offset-sm="4">
+            <div class="text-center my-2">
+              <label for="example-location" class="text-white">
                 <strong>Location:</strong>
               </label>
             </div>
             <b-form-select
-              class="text-center"
+              class="text-center "
               id="example-location"
             ></b-form-select>
           </b-col>
         </b-row>
 
         <!--  Details -->
-        <b-row class="my-3">
-          <b-col sm="4"></b-col>
-          <b-col sm="4">
+        <b-row class="text-center my-2">
+          <b-col sm="4" offset-sm="4">
             <div class="text-center">
-              <label for="example-i18n-picker" class="text-black">
+              <label for="example-i18n-picker" class="text-white">
                 <strong>Details:</strong>
               </label>
             </div>
           </b-col>
         </b-row>
         <b-form>
-          <b-row class="my-2">
-            <b-col sm="4"></b-col>
-            <b-col sm="4">
+          <b-row class="text-center">
+            <b-col sm="4" offset-sm="4">
               <b-form-textarea
-                class="text-center"
                 id="textarea-no-resize"
                 placeholder="Fixed height textarea"
-                rows="10"
+                rows="7"
                 no-resize
               ></b-form-textarea>
             </b-col>
           </b-row>
-          <b-row class="my-2">
-            <b-col sm="4"></b-col>
-            <b-col sm="4">
+          <b-row class="text-center my-2">
+            <b-col sm="4" offset-sm="4">
               <b-button type="submit" size="lg">Create</b-button>
             </b-col>
           </b-row>
@@ -227,9 +211,15 @@ export default {
 
 <style scoped>
 .body {
-  padding-top: 80px;
-  background: rgb(255,255,255);
-background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,190,113,1) 100%);
+  padding-top: 120px;
+  padding-bottom: 150px;
+   background: #f7f7f7;
+  background: rgb(63, 94, 251);
+  background: radial-gradient(
+    circle,
+    rgba(63, 94, 251, 1) 0%,
+    rgba(0, 0, 0, 1) 100%
+  );
 }
 /*.colorbutton{
  background: rgb(33,62,62);
--- WebFrontEnd/src/pages/Profile/Profile.vue
+++ WebFrontEnd/src/pages/Profile/Profile.vue
@@ -1,3 +1,162 @@
 <template>
-    <div></div>
+    <div>
+<div class="body" style="padding-bottom:60%">
+      <b-container>
+        <b-row class="text-center">
+            <b-col>
+        <h1 class="hhh">Profile</h1>
+        <br> <b-icon icon="person-fill" variant="light" font-scale="10"></b-icon>
+        </b-col>
+         </b-row>
+         <b-form @submit.prevent="onSubmit">
+          <b-row class="text-center">
+            <b-col sm="6" offset-sm="3">
+              <b-form-group
+                class="label"
+                id="username-input-group"
+                label="Username:"
+                label-for="username-input"
+              >
+                <b-form-input class="text-center"
+                  id="username-input"
+                  type="text"
+                  placeholder="Enter Username"
+                  required
+                  v-model="username"
+                  @blur="$v.username.$touch()"
+                ></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-row>
+           <b-row>
+
+            <b-col sm="6" offset-sm="3">
+              <b-form-group
+                class="text-center"
+                id="email-input-group"
+                label="Email address:"
+                label-for="email-input"
+                description="We'll never share your email with anyone else."
+              >
+                <b-form-input class="text-center"
+                  id="email-input"
+                  type="email"
+                  placeholder="Enter email"
+                  required
+                  v-model="email"
+                  @blur="$v.email.$touch()"
+                ></b-form-input>
+                <p v-if="!$v.email.email">Please provide a valid email address.</p>
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <b-row class="text-center">
+            <b-col sm="6" offset-sm="3">
+              <b-form-group
+                class="text-center"
+                id="password-input-group"
+                label="Password:"
+                label-for="password-input"
+              >
+                <b-form-input class="text-center"
+                  id="password-input"
+                  type="password"
+                  placeholder="Enter Password"
+                  required
+                  v-model="password"
+                ></b-form-input>
+              </b-form-group>
+               <b-button type="submit" variant="primary" :disabled="$v.$invalid">Update</b-button>
+            </b-col>
+          </b-row>
+
+        </b-form>
+      </b-container>
+         </div>
+    </div>
 </template>
+
+<script>
+
+import { email, sameAs } from 'vuelidate/lib/validators'
+// import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  validations: {
+    email: {
+      email
+      /*
+      unique: email => {
+        if (email === '') return true
+
+        TODO: Wait for DB
+        return axios.get('/users.json?orderBy="email"&equalTo="' + email + '"')
+          .then(res => {
+            return Object.keys(res.data).length === 0
+          })
+
+      } */
+    },
+    username: {
+      /*
+      unique: username => {
+        if (username === '') return true
+
+        TODO: Wait for DB
+        return axios.get('/users.json?orderBy="username"&equalTo="' + username + '"')
+          .then(res => {
+            return Object.keys(res.data).length === 0
+          })
+
+      } */
+    },
+    confirmPassword: {
+      sameAs: sameAs(vm => {
+        return vm.password
+      })
+    }
+  },
+  methods: {
+    onSubmit () {
+      const formData = {
+        email: this.email,
+        username: this.username,
+        password: this.password,
+        confirmPassword: this.confirmPassword
+      }
+      console.log(formData)
+      // TODO: Waiting for DB
+      // this.$store.dispatch('signup', formData)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.body {
+  background: #f7f7f7;
+  background: rgb(63, 94, 251);
+  background: radial-gradient(
+    circle,
+    rgba(63, 94, 251, 1) 0%,
+    rgba(0, 0, 0, 1) 100%
+  );
+}
+.hhh{
+  padding-top: 110px;
+  text-align: center;
+  color: white;
+}
+.text-center {
+  color: white;
+  text-align: center;
+}
+</style>
