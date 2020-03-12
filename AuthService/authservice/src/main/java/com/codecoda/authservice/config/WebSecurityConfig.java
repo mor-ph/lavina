7dd7e8bc5949bdcd4e366d1875e6ddf824bf9a01
Angel4 <angelkrastev96@gmail.com>
2020-03-12 11:28:14 +0200

    fix docker-compose

--- AuthService/authservice/src/main/java/com/codecoda/authservice/config/WebSecurityConfig.java
+++ AuthService/authservice/src/main/java/com/codecoda/authservice/config/WebSecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
 
 @Configuration
 @EnableWebSecurity
@@ -66,4 +69,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
+    
+
 }
