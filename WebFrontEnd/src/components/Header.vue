b2153ca596f9bcf2613e467840cff37a9fdde8cc
mor-ph <yet.an0ther.mortal@gmail.com>
2020-03-13 16:57:54 +0200

    header upd

--- WebFrontEnd/src/components/Header.vue
+++ WebFrontEnd/src/components/Header.vue
@@ -8,70 +8,73 @@
             <b-navbar-brand>LAVINA</b-navbar-brand>
           </router-link>
         </b-col>
-        <hide-at  breakpoint="small">
-          <b-col class="create md-4">
-            <b-navbar-nav>
-              <div class="cr">
-                <router-link to="/createEvent">
-                  <b-button class="yellowbtn mb-2" size="lg"> <!--size="lg"-->
-                    Create Event
-                    <b-icon icon="plus"></b-icon>
+        <b-col class="create cr" v-if="$mq === 'tablet' || $mq === 'desktop'">
+            <div ><!-- class="cr">-->
+              <!-- <b-col class="create"> -->
+                <b-navbar-nav>
+                  <div class="cr">
+                    <router-link to="/createEvent" class="aCr">
+                      <b-button class="yellowbtn mb-2" size="lg"> <!--size="lg"-->
+                        Create Event
+                        <b-icon icon="plus"></b-icon>
+                      </b-button>
+                    </router-link>
+                  </div>
+                </b-navbar-nav>
+              <!-- </b-col> -->
+            </div>
+        </b-col>
+        <b-col class="loginreg" v-if="$mq === 'tablet' || $mq === 'desktop'">
+            <div >
+                <router-link to="/login" v-if="!auth">
+                  <b-button class="yellowbtn">
+                    <strong>Login</strong>
                   </b-button>
                 </router-link>
-              </div>
-            </b-navbar-nav>
-          </b-col>
-        </hide-at>
-        <show-at :breakpoints="{small: 666, medium: 1000, large: 1600}" breakpoint="small">
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        </show-at>
-        <hide-at breakpoint="small">
-          <div>
-          <b-col class="loginreg">
-            <router-link to="/login" v-if="!auth">
-              <b-button class="yellowbtn">
-                <strong>Login</strong>
-              </b-button>
-            </router-link>
-            <router-link to="/register" v-if="!auth">
-              <b-button class="yellowbtn">
-                <strong>Register</strong>
-              </b-button>
-            </router-link>
-          </b-col>
-        </hide-at>
-          <show-at :breakpoints="{small: 666, medium: 1000, large: 1600}" breakpoint="small">
-            <b-collapse id="nav-collapse" is-nav right>
-              <!-- Right aligned nav items -->
-              <b-navbar-nav class="mr-auto">
-                <!-- <b-nav-item-dropdown right> -->
-                  <!-- Using 'button-content' slot -->
-                  <!-- <template v-slot:button-content>
-                    <em>
-                      <strong>{{getUser.username}}</strong>
-                    </em>
-                  </template> -->
-                  <b-dropdown-item to="profile"  v-if="auth">Profile
-                    <b-icon icon="person-fill"></b-icon>
-                  </b-dropdown-item>
-                  <b-dropdown-item @click="onLogout"  v-if="auth">Sign Out
-                    <b-icon icon="box-arrow-right"></b-icon>
-                  </b-dropdown-item>
-                  <b-dropdown-item to="home">All Events</b-dropdown-item>
-                  <b-dropdown-item to="CreateEvent">Create Event</b-dropdown-item>
-                <!-- </b-nav-item-dropdown> -->
-
-              </b-navbar-nav>
-            </b-collapse>
-            </show-at>
+                <router-link to="/register" v-if="!auth">
+                  <b-button class="yellowbtn">
+                    <strong>Register</strong>
+                  </b-button>
+                </router-link>
+            </div>
+        </b-col>
+        <b-navbar-toggle target="nav-collapse" v-if="$mq === 'mobile'"></b-navbar-toggle>
+        <!-- <div v-if="$mq === 'mobile'"> --><b-collapse id="nav-collapse" is-nav v-if="$mq === 'mobile'">
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="mr-auto dropNav" v-if="$mq === 'mobile'">
+                  <!-- <b-nav-item-dropdown target="nav-collapse"  right> -->
+                    <b-dropdown-item class="dItem" to="profile"  v-if="auth">Profile
+                      <b-icon icon="person-fill"></b-icon>
+                    </b-dropdown-item>
+                    <b-dropdown-item class="dItem" to="home">All Events</b-dropdown-item>
+                    <b-dropdown-item class="dItem" to="CreateEvent">Create Event</b-dropdown-item>
+                    <div class="dropdown-divider"></div>
+                    <b-dropdown-item class="dItem" @click="onLogout"  v-if="auth">Sign Out
+                      <b-icon icon="box-arrow-right"></b-icon>
+                    </b-dropdown-item>
+                    <b-dropdown-item class="dItem" to="/login" v-if="!auth">Log in</b-dropdown-item>
+                    <b-dropdown-item class="dItem" to="/register" v-if="!auth">Register</b-dropdown-item>
+                </b-navbar-nav>
+              </b-collapse>
+            <!-- </div> -->
+            <!-- </show-at> -->
       </b-navbar>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import { showAt, hideAt } from 'vue-breakpoints'
-import { mapGetters } from 'vuex'
+// import { showAt, hideAt } from 'vue-breakpoints'
+import Vue from 'vue'
+import VueMq from 'vue-mq'
+
+Vue.use(VueMq, {
+  breakpoints: {
+    mobile: 666,
+    tablet: 1000,
+    desktop: Infinity
+  }
+})
 
 export default {
   methods: {
@@ -83,13 +86,11 @@ export default {
   computed: {
     auth () {
       return this.$store.getters.isAuthenticated
-    },
-    ...mapGetters([
-      'getUser'
-    ])
-  },
-  components: { hideAt, showAt }
+    }
+  }
+  // components: { hideAt, showAt }
 }
+
 </script>
 
 <style scoped>
@@ -110,8 +111,21 @@ export default {
 }
  .cr{
   align-self: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-b-dropdown-item{
+.dropdown-item{
+   text-align: end;
+  color:ivory;
+}
+.aCr{
+  align-self: center;
+}
+#nav-collapse{
   text-align: end;
+}
+.dItem{
+  color:ivory;
 }
 </style>
