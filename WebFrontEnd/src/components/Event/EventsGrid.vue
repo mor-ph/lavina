b131d57f84bd74f825ad1489802c0babd5b7b3af
PetarParushev <lightone3591@gmail.com>
2020-03-24 20:18:37 +0200

    Revert "add pagination"
    This reverts commit c2efdc840b4fa5c4e64a2fabd84259138e13e258.

--- WebFrontEnd/src/components/Event/EventsGrid.vue
+++ WebFrontEnd/src/components/Event/EventsGrid.vue
@@ -1,41 +1,15 @@
 <template>
-<div>
     <div class="row">
-        <app-event id="events" :key="event.id" v-for="event in eventsArr" :event="event"></app-event>
+        <app-event :key="event.id" v-for="event in events" :event="event"></app-event>
     </div>
-    <b-pagination
-          v-model="currentPage"
-          :total-rows="rows"
-          :per-page="perPage"
-          aria-controls="events"
-          align="center"
-        ></b-pagination>
-</div>
 </template>
 
 <script>
 import Event from './Event'
 export default {
-  data () {
-    return {
-      currentPage: 1,
-      perPage: 12
-    }
-  },
   props: ['events'],
   components: {
     appEvent: Event
-  },
-  computed: {
-    eventsArr () {
-      return this.events.slice(
-        (this.currentPage - 1) * this.perPage,
-        this.currentPage * this.perPage
-      )
-    },
-    rows () {
-      return this.events.length
-    }
   }
 }
 </script>
